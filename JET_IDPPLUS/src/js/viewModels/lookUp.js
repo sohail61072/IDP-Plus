69513eb87e009c28c8167a816acaee450c6cbbc8
sohail61072 <44974926+sohail61072@users.noreply.github.com>
2020-10-26 12:53:59 +0000

    added lookup page

--- JET_IDPPLUS/src/js/viewModels/lookUp.js
+++ JET_IDPPLUS/src/js/viewModels/lookUp.js
@@ -7,13 +7,57 @@
 /**
  * lookUp module
  */
-define(['ojs/ojcore', 'knockout'
-], function (oj, ko) {
+define(['knockout', 'ojs/ojbootstrap', 'ojs/ojpagingdataproviderview', 'ojs/ojarraydataprovider', 'ojs/ojknockout', 'ojs/ojtable', 'ojs/ojpagingcontrol', 'ojs/ojbutton', 'ojs/ojpopup', 'ojs/ojanimation', 'ojs/ojformlayout', 'ojs/ojinputtext', 'ojs/ojselectcombobox', 'ojs/ojmenu', 'ojs/ojoption'
+], function (ko, Bootstrap, PagingDataProviderView, ArrayDataProvider, AnimationUtils) {
     /**
      * The view model for the main content view template
      */
     function lookUpContentViewModel() {
-        var self = this;
+        this.selectedMenuItem = ko.observable('(None selected yet)');
+    this.menuItemAction = function (event) {
+    this.selectedMenuItem(event.target.value);
+    }.bind(this);
+
+    this.startAnimationListener = function (event) {
+      var ui = event.detail;
+      if (event.target.id !== 'popup1') { return; }
+
+      if (ui.action === 'open') {
+        event.preventDefault();
+        var options = { direction: 'top' };
+        // AnimationUtils.slideIn(ui.element, options).then(ui.endCallback);
+      } else if (ui.action === 'close') {
+        event.preventDefault();
+        ui.endCallback();
+      }
+    };
+
+    this.openListener = function () {
+      var popup = document.getElementById('popup1');
+      popup.open('#btnGo');
+    };
+
+    this.cancelListener = function () {
+      var popup = document.getElementById('popup1');
+      popup.close();
+    };
+
+    var deptArray = [{LookupName:"Payment term",inputValue: "Mastek Email", transformedValue: 'Email' },
+    { LookupName:"Payment term",inputValue: "30 Days", transformedValue: 'Net 30' },
+    {LookupName:"Payment term",inputValue: "Mastek Email", transformedValue: 'Email' },
+    { LookupName:"Payment term",inputValue: "30 Days", transformedValue: 'Net 30' },
+    {LookupName:"Payment term",inputValue: "Mastek Email", transformedValue: 'Email' },
+    { LookupName:"Payment term",inputValue: "30 Days", transformedValue: 'Net 30' },
+    {LookupName:"Payment term",inputValue: "Mastek Email", transformedValue: 'Email' },
+    { LookupName:"Payment term",inputValue: "30 Days", transformedValue: 'Net 30' },
+    {LookupName:"Payment term",inputValue: "Mastek Email", transformedValue: 'Email' },
+    { LookupName:"Payment term",inputValue: "30 Days", transformedValue: 'Net 30' },
+    {LookupName:"Payment term",inputValue: "Mastek Email", transformedValue: 'Email' }
+ ];
+
+    this.pagingDataProvider = new PagingDataProviderView(new ArrayDataProvider(deptArray, { idAttribute: 'DepartmentId' }));
+  
+
     }
     
     return lookUpContentViewModel;
